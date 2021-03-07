5ece057b322bd15ff30746d39b932987631057a8
dennis <travlInfo4you@gmail.com>
2021-03-07 11:47:34 +0900

    두번째 부속품(fragment) 화면만 작성

--- app/src/main/java/kr/co/tjoeun/fragment_20210307/fragments/FirstFragment.kt
+++ app/src/main/java/kr/co/tjoeun/fragment_20210307/fragments/FirstFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_first.*
 import kr.co.tjoeun.fragment_20210307.R
 
 class FirstFragment : Fragment() {
@@ -15,5 +17,14 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        toastBtn.setOnClickListener {
+            Toast.makeText(activity,"프로그먼트를 통한 토스트", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
