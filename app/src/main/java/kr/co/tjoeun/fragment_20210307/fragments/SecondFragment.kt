7dd9e5dad580f03924b7dc11e32c525c7ba14c57
dennis <travlInfo4you@gmail.com>
2021-03-07 11:55:19 +0900

    두번째 프래그먼트 완성 (아직 사용은 안하고 있음)

---
+++ app/src/main/java/kr/co/tjoeun/fragment_20210307/fragments/SecondFragment.kt
@@ -0,0 +1,29 @@
+package kr.co.tjoeun.fragment_20210307.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_second.*
+import kr.co.tjoeun.fragment_20210307.R
+import java.util.zip.Inflater
+
+class SecondFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        secondToastBtn.setOnClickListener {
+            Toast.makeText(activity, "두번째 프래그먼트 토스트", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
