9058cb7e4e274e87bd456872ee26a94f4495b75a
dennis <travlInfo4you@gmail.com>
2021-03-07 11:21:04 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/fragment_20210307/fragments/FirstFragment.kt
@@ -0,0 +1,19 @@
+package kr.co.tjoeun.fragment_20210307.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kr.co.tjoeun.fragment_20210307.R
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+}
